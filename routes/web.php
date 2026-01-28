e8a9b70badca075642ef7fffca2db62f1c3b14eb
erickrodriguezbanda <rodriguezbandaerick@gmail.com>
2026-01-27 19:53:32 -0600

    Se agregó otra función a web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\PostController;
 Route::get('/hello', HomeController::class);
 Route::get('post/mensaje', [PostController::class, #Controlador seguido del método Mnensaje
 'Mensaje']);
-
+Route::get('post/about/{param}/{name?}', [PostController::class, 'About']);
 
 
 #Route::get('/', function () {
