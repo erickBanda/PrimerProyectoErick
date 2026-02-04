302db70c31cdbbcfcb7ee1e619b4aa7b0fc01cdb
erickrodriguezbanda <rodriguezbandaerick@gmail.com>
2026-02-03 19:49:44 -0600

    Se agrega la lista de empresa y se agrega un método a HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;    
+use App\Models\Pagina;
+use illuminate\Support\Facades\App;
+USE Yajra\Datatables\Datatables;
 
 class HomeController extends Controller
 {
+    public function empresa()
+    {
+        $datos["nombre"]= "Erick Rodriguez Banda";
+        $datos["fecha"]="2026-05-15";
+        $datos["actividad"]="Desarrollo de Software";
+        $datos["description_about"]="Empresa dedicada al desarrlollo de software a la medida de sus clientes";
+        $datos["texto_ejemplo"]="Aqui va la descripcion del texto de ejemplo";
+
+        $usuarios=new Pagina();
+        $dats["listausuarios"]=$usuarios->ObtenerListado();
+        return view('empresa', $datos);
+    }    
+
     public function __invoke() #Este método se llama cuando se usa el controlador como una sola acción
     {
         return view('hello');
     }
+    
 }
